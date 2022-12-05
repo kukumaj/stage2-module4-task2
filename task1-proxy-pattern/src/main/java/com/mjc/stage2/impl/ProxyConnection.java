172b871322d4805aa447dfb19d11fab6f5772d96
kukumaj <zakumaj@gmail.com>
2022-12-05 22:27:59 +0100

    done

--- task1-proxy-pattern/src/main/java/com/mjc/stage2/impl/ProxyConnection.java
+++ task1-proxy-pattern/src/main/java/com/mjc/stage2/impl/ProxyConnection.java
@@ -1,7 +1,8 @@
 package com.mjc.stage2.impl;
 
+import com.mjc.stage2.Connection;
 
-public class ProxyConnection {
+public class ProxyConnection implements Connection {
     private RealConnection realConnection;
 
     public ProxyConnection(RealConnection realConnection) {
@@ -9,7 +10,16 @@ public class ProxyConnection {
     }
 
     public void reallyClose() {
-        // Write your code here!
+        realConnection.close();
     }
-    // Implement methods here!
+
+    @Override
+    public void close() {
+        ConnectionPool connectionPool = ConnectionPool.getInstance();
+        connectionPool.releaseConnection(this);
+    }
+
+    public boolean isClosed() {
+        return realConnection.isClosed();
+    }
 }
